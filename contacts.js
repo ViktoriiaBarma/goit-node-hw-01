38e5306078a8227e3773ad2a376bffd705d05891
ViktoriiaBarma <barma.viktoria@gmail.com>
2023-09-09 15:47:00 +0300

    add files

---
+++ contacts.js
@@ -0,0 +1,63 @@
+const fs = require('fs').promises;
+const path = require('path');
+const { randomUUID } = require('crypto');
+
+const contactsPath = path.join(__dirname, "db", "contacts.json")
+
+const writeContacts = async (contacts) => {
+    return await fs.writeFile (contactsPath, JSON.stringify(contacts, null,2))
+}
+
+
+async function listContacts() { 
+    const data = await fs.readFile(contactsPath)
+    return JSON.parse(data)
+}
+
+
+async function getContactById(contactId) {
+    const contacts = await listContacts()
+    const contact = contacts.find((contact) => contact.id === contactId)
+    
+    return contact
+ }
+
+async function removeContact(contactId) { 
+    const contacts = await listContacts()
+    const index = contacts.findIndex((contact) => contact.id === contactId)
+    
+    if (index === -1) {
+        return null
+    }
+
+    const [result] = contacts.splice(index, 1);
+    
+    await writeContacts(contacts);
+    
+    return [result]
+
+}
+
+async function addContact(name, email, phone) { 
+        const contacts = await listContacts()
+    const newContact = {
+        id: randomUUID(),
+        name,
+        email,
+        phone,
+    }
+    
+    contacts.push(newContact);
+
+    await writeContacts(contacts);
+
+    return newContact
+}
+
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
